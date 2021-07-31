65cf0a5c9e5040bdf581673d566ef4e7e12559ea
biancachiusano <72268568+biancachiusano@users.noreply.github.com>
2021-07-31 12:43:19 +0200

    Update animation1.swift

--- Practice/Animation/animation1.swift
+++ Practice/Animation/animation1.swift
@@ -12,3 +12,15 @@ struct ContentView: View {
           .animation(.spring(), value: angle)
       }
 }
+
+//Interpolating Spring
+    @State private var angle: Double = 0
+
+    var body: some View {
+        Button("Press here") {
+            angle += 45
+        }
+        .padding()
+        .rotationEffect(.degrees(angle))
+        .animation(.interpolatingSpring(mass: 1, stiffness: 1, damping: 0.5, initialVelocity: 10), value: angle)
+    }
